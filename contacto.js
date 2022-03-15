8db493276d68f195384ee0c4c26b9a106452b409
dhinogz <davidhinojosauni@gmail.com>
2022-03-14 23:25:26 -0600

    Format code

--- contacto.js
+++ contacto.js
@@ -1,27 +1,27 @@
-const checkbox = document.getElementById('show');
+const checkbox = document.getElementById("show");
 
-const box = document.getElementById('box');
+const box = document.getElementById("box");
 const email = document.getElementById("email");
 
-checkbox.addEventListener('click', function handleClick() {
+checkbox.addEventListener("click", function handleClick() {
   if (checkbox.checked) {
-    box.style.display = 'block';
+    box.style.display = "block";
   } else {
-    box.style.display = 'none';
+    box.style.display = "none";
   }
 });
 
-function enableSubmitExistente(){
-  let inputs = document.getElementsByClassName('req');
-  let btn = document.getElementById('envio');
+function enableSubmitExistente() {
+  let inputs = document.getElementsByClassName("req");
+  let btn = document.getElementById("envio");
   let isValid = true;
 
-  for (var i = 0; i < inputs.length; i++){
-      let changedInput = inputs[i];
-      if (changedInput.value.trim() === "" || changedInput.value === null){
-          isValid = false;
+  for (var i = 0; i < inputs.length; i++) {
+    let changedInput = inputs[i];
+    if (changedInput.value.trim() === "" || changedInput.value === null) {
+      isValid = false;
       break;
-      }
+    }
   }
   btn.disabled = !isValid;
 }
